4a52ce971cb54fa1cc08e22f4b3dd78f801cd458
hlynur92 <43953815+hlynur92@users.noreply.github.com>
2019-05-03 12:40:35 +0200

    latest update

--- presentation/checkout.php
+++ presentation/checkout.php
@@ -5,80 +5,101 @@
 <body>
 
 <?php include 'partials/navigation.php';?>
+<?php include '../business/ProductController.php'?>
+<?php
+$productcontroller = new ProductController();
+
+$products = $productcontroller->getCartProducts();
+?>
 
 
 <main role="main">
-
     <div class="container mb-5">
         <div class="py-5 text-center">
             <h2>Checkout form</h2>
             <p class="lead">Below is an example form built entirely with Bootstrap's form controls. Each required form group has a validation state that can be triggered by attempting to submit the form without completing it.</p>
         </div>
-
         <div class="row">
             <div class="col-md-4 order-md-2 mb-4">
                 <h4 class="d-flex justify-content-between align-items-center mb-3">
                     <span class="text-muted">Your cart</span>
-                    <span class="badge badge-secondary badge-pill">3</span>
+                    <span class="badge badge-secondary badge-pill"><?php echo count($_SESSION['cart'])?></span>
                 </h4>
                 <ul class="list-group mb-3">
-                    <li class="list-group-item d-flex justify-content-between lh-condensed">
-                        <div>
-                            <h6 class="my-0">Product name</h6>
-                            <small class="text-muted">Brief description</small>
-                        </div>
-                        <span class="text-muted">$12</span>
-                    </li>
-                    <li class="list-group-item d-flex justify-content-between lh-condensed">
-                        <div>
-                            <h6 class="my-0">Second product</h6>
-                            <small class="text-muted">Brief description</small>
-                        </div>
-                        <span class="text-muted">$8</span>
-                    </li>
-                    <li class="list-group-item d-flex justify-content-between lh-condensed">
-                        <div>
-                            <h6 class="my-0">Third item</h6>
-                            <small class="text-muted">Brief description</small>
-                        </div>
-                        <span class="text-muted">$5</span>
-                    </li>
-                    <li class="list-group-item d-flex justify-content-between bg-light">
-                        <div class="text-success">
-                            <h6 class="my-0">Promo code</h6>
-                            <small>EXAMPLECODE</small>
-                        </div>
-                        <span class="text-success">-$5</span>
-                    </li>
+                    <?php
+                    foreach ($products as $product){
+                        foreach($_SESSION['cart'] as $item){
+                            if ($product['ProductID'] == $item['productid']){
+                                    if ($item['specialofferid'] == null){
+                                        $price = $product['Price'] * $item['quantity'];
+                                        $template = "
+                                        <li class=\"list-group-item d-flex justify-content-between lh-condensed\">
+                                            <div>
+                                                <h6 class=\"my-0\">" . $product['ProductName'] . "</h6>
+                                                <small class=\"text-muted\">" . $product['Description'] . "</small>
+                                            </div>
+                                            <span class=\"text-muted\">" . $price . "</span>
+                                        </li>";
+                                        echo $template;
+                                    }elseif ($item['specialofferid'] != null){
+                                        $discount = $product['Discount']/100;
+                                        $discountPrice = $product['Price'] * ( 1 - $discount);
+                                        $discountPrice *= $item['quantity'];
+                                        $template = "
+                                        <li class=\"list-group-item d-flex justify-content-between lh-condensed\">
+                                            <div>
+                                                <h6 class=\"my-0\">" . $product['ProductName'] . "</h6>
+                                                <small class=\"text-muted\">" . $product['Description'] . "</small>
+                                            </div>
+                                            <span class=\"text-muted\">" . round($discountPrice, 2) . "</span>
+                                        </li>";
+                                        echo $template;
+                                    }
+                                }
+                            }
+                        }
+                    ?>
+
                     <li class="list-group-item d-flex justify-content-between">
                         <span>Total (USD)</span>
-                        <strong>$20</strong>
+                        <strong><?php
+                            $subtotal = 0;
+                            foreach ($products as $product){
+                                foreach($_SESSION['cart'] as $item){
+                                    if ($product['ProductID'] == $item['productid']){
+                                        if($item['specialofferid'] == null){
+                                            $price = $product['Price'] * $item['quantity'];
+                                            $subtotal += $price;
+                                        }
+                                        if($item['specialofferid'] != null){
+                                            $discount = $product['Discount']/100;
+                                            $discountPrice = $product['Price'] * ( 1 - $discount);
+                                            $discountPrice *= $item['quantity'];
+                                            $subtotal += round($discountPrice, 2);
+                                        }
+                                    }
+                                }
+                            }
+                            echo round($subtotal, 2) . " €";
+                            ?></strong>
                     </li>
                 </ul>
 
-                <form class="card p-2">
-                    <div class="input-group">
-                        <input type="text" class="form-control" placeholder="Promo code">
-                        <div class="input-group-append">
-                            <button type="submit" class="btn btn-secondary">Redeem</button>
-                        </div>
-                    </div>
-                </form>
             </div>
             <div class="col-md-8 order-md-1">
                 <h4 class="mb-3">Billing address</h4>
-                <form class="needs-validation" novalidate="">
+                <form method="post" class="needs-validation" novalidate="">
                     <div class="row">
                         <div class="col-md-6 mb-3">
                             <label for="firstName">First name</label>
-                            <input type="text" class="form-control" id="firstName" placeholder="" value="" required="">
+                            <input type="text" class="form-control" name="firstname" id="firstName" placeholder="" value="" required="">
                             <div class="invalid-feedback">
                                 Valid first name is required.
                             </div>
                         </div>
                         <div class="col-md-6 mb-3">
                             <label for="lastName">Last name</label>
-                            <input type="text" class="form-control" id="lastName" placeholder="" value="" required="">
+                            <input type="text" class="form-control" name="lastname" id="lastName" placeholder="" value="" required="">
                             <div class="invalid-feedback">
                                 Valid last name is required.
                             </div>
@@ -86,21 +107,8 @@
                     </div>
 
                     <div class="mb-3">
-                        <label for="username">Username</label>
-                        <div class="input-group">
-                            <div class="input-group-prepend">
-                                <span class="input-group-text">@</span>
-                            </div>
-                            <input type="text" class="form-control" id="username" placeholder="Username" required="">
-                            <div class="invalid-feedback" style="width: 100%;">
-                                Your username is required.
-                            </div>
-                        </div>
-                    </div>
-
-                    <div class="mb-3">
-                        <label for="email">Email <span class="text-muted">(Optional)</span></label>
-                        <input type="email" class="form-control" id="email" placeholder="you@example.com">
+                        <label for="email">Email</label>
+                        <input type="email" class="form-control" name="email" id="email" placeholder="you@example.com">
                         <div class="invalid-feedback">
                             Please enter a valid email address for shipping updates.
                         </div>
@@ -108,15 +116,10 @@
 
                     <div class="mb-3">
                         <label for="address">Address</label>
-                        <input type="text" class="form-control" id="address" placeholder="1234 Main St" required="">
+                        <input type="text" class="form-control" name="address" id="address" placeholder="1234 Main St" required="">
                         <div class="invalid-feedback">
                             Please enter your shipping address.
                         </div>
-                    </div>
-
-                    <div class="mb-3">
-                        <label for="address2">Address 2 <span class="text-muted">(Optional)</span></label>
-                        <input type="text" class="form-control" id="address2" placeholder="Apartment or suite">
                     </div>
 
                     <div class="row">
@@ -130,16 +133,7 @@
                                 Please select a valid country.
                             </div>
                         </div>
-                        <div class="col-md-4 mb-3">
-                            <label for="state">State</label>
-                            <select class="custom-select d-block w-100" id="state" required="">
-                                <option value="">Choose...</option>
-                                <option>California</option>
-                            </select>
-                            <div class="invalid-feedback">
-                                Please provide a valid state.
-                            </div>
-                        </div>
+
                         <div class="col-md-3 mb-3">
                             <label for="zip">Zip</label>
                             <input type="text" class="form-control" id="zip" placeholder="" required="">
@@ -148,16 +142,6 @@
                             </div>
                         </div>
                     </div>
-                    <hr class="mb-4">
-                    <div class="custom-control custom-checkbox">
-                        <input type="checkbox" class="custom-control-input" id="same-address">
-                        <label class="custom-control-label" for="same-address">Shipping address is the same as my billing address</label>
-                    </div>
-                    <div class="custom-control custom-checkbox">
-                        <input type="checkbox" class="custom-control-input" id="save-info">
-                        <label class="custom-control-label" for="save-info">Save this information for next time</label>
-                    </div>
-                    <hr class="mb-4">
 
                     <h4 class="mb-3">Payment</h4>
 
@@ -170,11 +154,8 @@
                             <input id="debit" name="paymentMethod" type="radio" class="custom-control-input" required="">
                             <label class="custom-control-label" for="debit">Debit card</label>
                         </div>
-                        <div class="custom-control custom-radio">
-                            <input id="paypal" name="paymentMethod" type="radio" class="custom-control-input" required="">
-                            <label class="custom-control-label" for="paypal">Paypal</label>
-                        </div>
                     </div>
+
                     <div class="row">
                         <div class="col-md-6 mb-3">
                             <label for="cc-name">Name on card</label>
@@ -209,11 +190,10 @@
                         </div>
                     </div>
                     <hr class="mb-4">
-                    <button onclick="window.location.href = 'payment.php';" class="btn btn-primary btn-lg btn-block" type="submit">Continue to checkout</button>
+                    <button onclick="window.location.href = 'payment.php';" class="btn btn-primary btn-lg btn-block" type="submit" name="checkout">Continue to checkout</button>
                 </form>
             </div>
         </div>
-
     </div>
 </main>
 <?php include '../presentation/partials/footer.php';?>
